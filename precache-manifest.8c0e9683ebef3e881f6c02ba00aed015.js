5acfc47b524e4e23e15425c977c74c4699041ef0
webmagic123 <30417155+webmagic123@users.noreply.github.com>
2019-04-02 08:27:50 -0700

    Updates

---
+++ precache-manifest.8c0e9683ebef3e881f6c02ba00aed015.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1493f6986029e98f237e",
+    "url": "/webmagic123/react-styled-component-challenge/static/js/runtime~main.dbabbaed.js"
+  },
+  {
+    "revision": "a8066e865aa080b9a0b0",
+    "url": "/webmagic123/react-styled-component-challenge/static/js/main.0199430b.chunk.js"
+  },
+  {
+    "revision": "eb3190c97dfc6eb69973",
+    "url": "/webmagic123/react-styled-component-challenge/static/js/2.3a4421ec.chunk.js"
+  },
+  {
+    "revision": "a8066e865aa080b9a0b0",
+    "url": "/webmagic123/react-styled-component-challenge/static/css/main.7f5e2d7d.chunk.css"
+  },
+  {
+    "revision": "d6a50190d33a3bf217612f0ea185b0dc",
+    "url": "/webmagic123/react-styled-component-challenge/index.html"
+  }
+];
