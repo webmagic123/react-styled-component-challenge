a05aee905e149c651d9899a8e270ad8ce98e49d4
webmagic123 <30417155+webmagic123@users.noreply.github.com>
2019-04-02 08:31:24 -0700

    Updates

---
+++ precache-manifest.71b1e113314f52699453f2b68dec6eeb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c2db854b1d125abccf7",
+    "url": "/react-styled-component-challenge/static/js/runtime~main.5ed72a01.js"
+  },
+  {
+    "revision": "48df4afc6b872a751bdc",
+    "url": "/react-styled-component-challenge/static/js/main.ebc05634.chunk.js"
+  },
+  {
+    "revision": "74313364bdf7dc08b7d6",
+    "url": "/react-styled-component-challenge/static/js/2.c96e6715.chunk.js"
+  },
+  {
+    "revision": "48df4afc6b872a751bdc",
+    "url": "/react-styled-component-challenge/static/css/main.7f5e2d7d.chunk.css"
+  },
+  {
+    "revision": "ec3791063ae6ac2c63a82cfbc12fc8da",
+    "url": "/react-styled-component-challenge/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/webmagic123/react-styled-component-challenge/precache-manifest.8c0e9683ebef3e881f6c02ba00aed015.js"
+  "/react-styled-component-challenge/precache-manifest.71b1e113314f52699453f2b68dec6eeb.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/webmagic123/react-styled-component-challenge/index.html", {
+workbox.routing.registerNavigationRoute("/react-styled-component-challenge/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
